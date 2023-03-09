64ef7e793291721309157b59ff6f721b1e1dc862
attthurjaanu <janur9862@gmail.com>
2023-03-09 16:13:34 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def dhoni(request):
+    return HttpResponse('MSD is the best finisher in the world')
+
